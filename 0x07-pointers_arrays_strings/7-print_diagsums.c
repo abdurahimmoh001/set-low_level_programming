0d0be6d4d519184b1a3a774379e0f6e36f929af9
abdurahimmoh001 <abdurahimmohammed001@gmail.com>
2026-06-17 23:51:19 -0700

    fixed it

--- 0x07-pointers_arrays_strings/7-print_diagsums.c
+++ 0x07-pointers_arrays_strings/7-print_diagsums.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_chessboard - a function that prints chessboard
- * @a: matrix
- * Return: nothing
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: pointer to the first element of the matrix
+ * @size: size of the square matrix
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
+	int diag1 = 0;
+	int diag2 = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-			if (j == 7)
-				_putchar('\n');
-		}
+		diag1 += a[i * size + i];
+		diag2 += a[i * size + (size - 1 - i)];
 	}
+
+	printf("%d, %d\n", diag1, diag2);
 }
