d48eb0243eb2fe033ad1eac1ad0d438af21dc742
abdurahimmoh001 <abdurahimmohammed001@gmail.com>
2026-06-21 06:53:42 -0700

    one

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+/* File: 2-strlen_recursion.c */
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to measure
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
