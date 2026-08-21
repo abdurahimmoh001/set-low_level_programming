c82f98da2176743d9e0e3c9a40e824c47faad83d
Abdurahim Mohammed <abdurahimmohammed001@gmail.com>
2026-08-21 11:46:24 +0300

    SET

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - create array
+ * @min: starting number
+ * @max: last number
+ * Return: int *
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, j;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(sizeof(int) * ((max - min) + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	j = min;
+	for (i = 0; i < ((max - min) + 1); i++)
+	{
+		ptr[i] = j;
+		j++;
+	}
+
+	return (ptr);
+}
